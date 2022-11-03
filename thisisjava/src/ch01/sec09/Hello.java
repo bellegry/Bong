0b263112735cd284580641e2ea0b840733839774
SolbridPlus <SolbridPlus@DESKTOP-I784IQA>
2022-11-03 11:13:27 +0900

    first commit.

---
+++ thisisjava/src/ch01/sec09/Hello.java
@@ -0,0 +1,20 @@
+package ch01.sec09;
+
+/**
+ * 
+ * @author SolbridPlus
+ *
+ */
+
+/*
+장제목 : 1장 자바 시작하기
+작성일 : 2022. 11. 03
+ */
+
+public class Hello {
+	// 프로그램 실행 진입점
+	public static void main(String[] args) {
+		// 콘솔에 출력하는 실행문
+		System.out.println("Hello Java");
+	}
+}
